b6de3a5a1dd5907757685281529ffc4772408671
pelle <pellep@sina.com>
2017-12-08 15:16:13 +0800

    init

--- wechat.v1.0/src/config/config.go
+++ wechat.v1.0/src/config/config.go
@@ -1,3 +1,16 @@
 package config
-
+//版本
 const VERSION="1.0"
+//微信配置
+type WechatItem struct{
+	Appid string `json:"appid"`
+	Appkey string `json:"appkey"`
+	InterfaceToken string `json:"token"`
+}
+
+var WechatConfig  map[string]*WechatItem = map[string]*WechatItem{
+		"top_learn":&WechatItem{"wxb52ac09696b62641","5df4e9762fdbc14e1caa2995122fe5cd","top_learn_token"}}
+
+
+func init() {
+}
--- wechat.v1.0/src/main.go
+++ wechat.v1.0/src/main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"config"
+	//"encoding/json"
+	"net/http"
+	"io"
+	"sort"
+	"crypto/sha1"
 )
 
+func WechatHandle(w http.ResponseWriter,r *http.Request){
+	r.ParseForm();
+	if r.Method == "GET"{
+		fieldlist := []string{"account","signature","timestamp","nonce","echostr"}
+		for _,v := range fieldlist{
+			value := r.Form[v]
+			if value == nil{
+				io.WriteString(w,"Miss param: "+v)
+				return
+			}
+		}
+		account := r.FormValue("account")
+		signature := r.FormValue("signature")
+		timestamp := r.FormValue("timestamp")
+		nonce := r.FormValue("nonce")
+		echostr := r.FormValue("echostr")
+
+		if config.WechatConfig[account] == nil {
+			io.WriteString(w,"Wrong param: account")
+			return
+		}
+
+		keys := []string{config.WechatConfig[account].InterfaceToken,timestamp,nonce}
+		sort.Strings(keys)
+		strsha1 := ""
+		for _,v := range keys {
+			strsha1 += v
+		}
+		h := sha1.New()
+		h.Write([]byte(strsha1))
+		shabyte := h.Sum(nil)
+		if string(shabyte[:]) == signature {
+			io.WriteString(w,echostr)
+		} else {
+			io.WriteString(w,"")
+		}
+	} else if r.Method == "POST" {
+
+	}
+}
 func main() {
-	fmt.Println("Hello world!")
-	fmt.Println("Version "+config.VERSION)
-	fmt.Printf("%s",config.VERSION)
+	http.HandleFunc("/wechat",WechatHandle)
+	http.ListenAndServe(":3000",nil)
 }
