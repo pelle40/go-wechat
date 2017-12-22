aaf462beae32bee3689f772b1820cdffc97ab5e5
pelle <pellep@sina.com>
2017-12-22 11:23:54 +0800

    cache

--- wechat.v1.0/src/cache/cache.go
+++ wechat.v1.0/src/cache/cache.go
@@ -1,16 +1,19 @@
-package cache
+package main
 //版本
-const VERSION = "1.0"
-//消息队列长度
-const CAPACITY = 1024
+//const VERSION = "1.0"
 
 import (
+	"os"
 	"net"
 	"fmt"
+	"path"
+	"time"
 	"errors"
-	"io/ioutil"
 	"regexp"
 	"strings"
+	"os/exec"
+	"io/ioutil"
+	"path/filepath"
 )
 
 type Request struct{
@@ -25,21 +28,33 @@ type Cache struct{
 	Data map[string]string
 	//处理请求chan
 	ReqC chan Request
+	//更新数据到文件
+	UpFlag chan int
+	//数据文件
+	dataFile string
 }
 
-func (cache *Cache)Start(ntp,addr string){
+func (cache *Cache)Start(ntp,addr,datafile string,capcity int){
 	//建立连接
 	tcpAddr,err := net.ResolveTCPAddr(ntp,addr)
 	cache.checkerr(err)
 	tcpListener,err := net.ListenTCP(ntp,tcpAddr)
 	cache.checkerr(err)
-	cache.ReqC = make(chan Request,CAPACITY)
+	cache.ReqC = make(chan Request,capcity)
 	cache.Data = make(map[string]string)
+	cache.UpFlag = make(chan int,1)
+	cache.dataFile = datafile
 	go func() {
 		for {
 			cache.getData()
 		}
 	}()
+	go func() {
+		for{
+			_ := <-cache.UpFlag
+			cache.updateDataToFile()
+		}
+	}
 	for {
 		conn,err := tcpListener.Accept()
 		cache.checkerr(err)
@@ -106,16 +121,49 @@ func (cache *Cache)getData() {
 		}
 	} else if request.method == "set" {
 		cache.Data[request.key] = request.value
+		cache.UpFlag <- 1
 		request.ch <- "ok"
 	} else if request.method == "delete" {
 		if _,ok := cache.Data[request.key];ok {
 			delete(cache.Data,request.key)
 		}
+		cache.UpFlag <- 1
 		request.ch <- "ok"
 	}
 }
+func (cache *Cache)updateDataToFile() {
+	//数据文件
+	datafile,err := os.OpenFile(cache.dataFile,os.O_TRUNC,0666)
+	if err!=nil{
+		fmt.Println("Create or Open data file error!")
+		return
+	}
+	for k,v := range cache.Data{
+		datafile.WriteString(k+","+v+"\n")
+	}
+	datafile.Close()
+}
 
-//func main() {
-//	cache := Cache{}
-//	cache.Start("tcp",":3001")
-//}
+func main() {
+	folder,_ := os.Getwd()
+	if os.Getppid()!=1{
+		filepath,_ := filepath.Abs(os.Args[0])
+		cmd := exec.Command(filepath)
+		//日志文件
+		filename := path.Join(folder,"log"+time.Now().Format("2006-01-02")+".log")
+		openfile,err := os.OpenFile(filename,os.O_CREATE,0666)
+		if err!=nil{
+			return nil,errors.New("Create file error")
+		}
+		defer logfile.Close()
+		//标准输入输出
+		cmd.Stdin = logfile
+		cmd.Stdout = logfile
+		cmd.Stderr = logfile
+		cmd.Start()
+		return
+	}
+	cache := Cache{}
+	datafilename := path.Join(folder,'data')
+	cache.Start("tcp",":3001",datafilename,1024)
+}
